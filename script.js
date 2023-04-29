4787bc40e97dd1694d2e6fa545e46d3addf5e4ef
skyisnotmylimit <98826786+skyisnotmylimit@users.noreply.github.com>
2023-04-30 04:05:35 +0530

    30th last

--- script.js
+++ script.js
@@ -10,13 +10,10 @@ searchBtn.addEventListener("click",()=>{
   p.then((response)=>{
     return response.json()
   }).then((data)=>{
-    console.log(data)
     document.querySelector(".city").innerHTML = data.name
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp)+" °C"
     document.querySelector(".humidity").innerHTML = data.main.humidity+"%"
     document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h"
     weatherIcon.src = "images/"+data.weather[0].main+".png"
-    console.log("images/"+data.weather[0].main+".png")
-    console.log(data.weather[0].main)
   })
 })
